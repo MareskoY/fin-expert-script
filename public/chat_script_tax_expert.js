af7ced1affeab4f4865c76c0c3aff09abf91b3ac
Mark Hinschberger <markhinschberger@gmail.com>
2025-01-24 11:14:20 +0000

    always fetch jwt

--- public/chat_script_tax_expert.js
+++ public/chat_script_tax_expert.js
@@ -346,10 +346,13 @@ async function setupIframe({ token, domain, chatIframe }) {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     console.log('Setting up iframe for Safari:', isSafari);
 
+
     if (isSafari) {
         // Try to get JWT (either from storage or by fetching)
         let jwt = localStorage.getItem('auth_token');
-        if (!jwt) {
+
+        // always fetch for now
+        if (true) {
             jwt = await getJWT({ token, domain });
         }
 
